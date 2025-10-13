0122cdd9b4a44a119bffadc090fc2fcac903947d
balipusy25 <balipusy2@gmail.com>
2025-10-13 14:44:46 +0300

    Create config.example.js

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// config.example.js
+// COPY to config.js and replace placeholders with real credentials
+export const CONFIG = {
+  PAYPAL_CLIENT_ID: "REPLACE_WITH_PAYPAL_CLIENT_ID", // sandbox or live
+};
